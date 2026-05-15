dd3d952a2bc00315697a44d9d781b6bd897348c9
Kaweeshwara <githubkawe@gmail.com>
2026-05-15 14:58:17 +0530

    feat: add abstract User base class with encapsulation and abstraction

---
+++ src/main/java/com/eventbooking/model/User.java
@@ -0,0 +1,66 @@
+package com.eventbooking.model;
+
+public abstract  class User {
+
+
+        // private fields - ENCAPSULATION
+        private String userId;
+        private String name;
+        private String email;
+        private String password;
+        private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public User(String userId, String name, String email, String password, String phone) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
+    public abstract String getRole();
+
+    @Override
+    public String toString() {
+        return userId + "|" + name + "|" + email + "|" + password + "|" + phone + "|" + getRole();
+    }
+}
